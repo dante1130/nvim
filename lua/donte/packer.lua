ef74b18ce497e97532fb375537054bceb0d2dc6e
dante1130 <andrewdante2@gmail.com>
2023-05-27 11:00:13 +0800

    Setup lsp bindings

--- lua/donte/packer.lua
+++ lua/donte/packer.lua
@@ -9,7 +9,7 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    use({	"catppuccin/nvim", as = "catppuccin" })
+    use({"catppuccin/nvim", as = "catppuccin" })
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use('nvim-treesitter/playground')
