f896726fc9e4551e220fb4d8efacd27cdb47da67
dante1130 <andrewdante2@gmail.com>
2023-06-05 15:26:14 +0800

    Clippy on save

--- after/plugin/rusttools.lua
+++ after/plugin/rusttools.lua
@@ -12,6 +12,14 @@ local opts = {
             -- Code action groups
             vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
         end,
+
+        settings = {
+            ["rust-analyzer"] = {
+                checkOnSave = {
+                    command = "clippy"
+                },
+            }
+        }
     },
 
     dap = {
