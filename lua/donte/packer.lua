4b25ab84b955ec1a55a9517f474767cfc3f855f0
dante1130 <andrewdante2@gmail.com>
2023-06-11 19:42:13 +0800

    Add comment.nvim

--- lua/donte/packer.lua
+++ lua/donte/packer.lua
@@ -69,4 +69,11 @@ return require('packer').startup(function(use)
         'glacambre/firenvim',
         run = function() vim.fn['firenvim#install'](0) end
     }
+
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
 end)
