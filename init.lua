6b4cd7d434a98d38f02b1bf2fe018880d8228f9e
dante1130 <andrewdante2@gmail.com>
2024-04-07 15:17:20 +0800

    set format keybind

--- init.lua
+++ init.lua
@@ -460,6 +460,8 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
+  nmap('<leader>fmt', vim.lsp.buf.format, '[F]or[m]a[t]')
 end
 
 -- document existing key chains
